a766810522b44e346deb3fde005b5e1a94148c2b
Zishan Ahmad <zishan.barun@gmail.com>
2024-07-23 21:57:11 +0530

    updated deploy url

--- src/views/EmbeddedChat/EmbeddedChat.js
+++ src/views/EmbeddedChat/EmbeddedChat.js
@@ -6,8 +6,8 @@ const ECComponent = ({ isEcOpen, setIsECOpen }) => {
   return (
     <div className={styles.ecContainer}>
       <EmbeddedChat
-        host="https://spiral-memory.rocket.chat"
-        roomId="6679b390ef5048f098b06d48"
+        host="https://demo.qa.rocket.chat"
+        roomId="669fd7388129fb4346981692"
         anonymousMode
         theme={StormySeas}
         height="85vh"
@@ -15,7 +15,7 @@ const ECComponent = ({ isEcOpen, setIsECOpen }) => {
         auth={{
           flow: "TOKEN",
           credentials: {
-            resume: "czWwA8NzkBkch7lzl3G-yc6Pm5Gqco9PBf-mu4CyLqk",
+            resume: "Pvjmwrv5DTr6yIA4bT4LhtDR86eQ08yIwWyePTpmhqR",
           },
         }}
         isClosable={true}
